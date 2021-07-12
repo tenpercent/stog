33f197f664caa730bbad56aaef8348b0ac03c8b1
max <maksim@cs.fsu.edu>
2021-07-12 15:51:23 -0400

    add conversion from string into amr object, copying the logic from file IO

--- stog/data/dataset_readers/amr_parsing/io.py
+++ stog/data/dataset_readers/amr_parsing/io.py
@@ -51,6 +51,48 @@ class AMRIO:
                 yield amr
 
     @staticmethod
+    def read_str(source_str):
+        amr = AMR()
+        graph_lines = []
+        misc_lines = []
+        for line in source_str.split('\n'):
+            line = line.rstrip()
+            if line == '':
+                if len(graph_lines) != 0:
+                    amr.graph = AMRGraph.decode(' '.join(graph_lines))
+                    amr.graph.set_src_tokens(amr.get_src_tokens())
+                    amr.misc = misc_lines
+                    yield amr
+                    amr = AMR()
+                graph_lines = []
+                misc_lines = []
+            elif line.startswith('# ::'):
+                if line.startswith('# ::id '):
+                    amr.id = line[len('# ::id '):]
+                elif line.startswith('# ::snt '):
+                    amr.sentence = line[len('# ::snt '):]
+                elif line.startswith('# ::tokens '):
+                    amr.tokens = json.loads(line[len('# ::tokens '):])
+                elif line.startswith('# ::lemmas '):
+                    amr.lemmas = json.loads(line[len('# ::lemmas '):])
+                elif line.startswith('# ::pos_tags '):
+                    amr.pos_tags = json.loads(line[len('# ::pos_tags '):])
+                elif line.startswith('# ::ner_tags '):
+                    amr.ner_tags = json.loads(line[len('# ::ner_tags '):])
+                elif line.startswith('# ::abstract_map '):
+                    amr.abstract_map = json.loads(line[len('# ::abstract_map '):])
+                else:
+                    misc_lines.append(line)
+            else:
+                graph_lines.append(line)
+
+        if len(graph_lines) != 0:
+            amr.graph = AMRGraph.decode(' '.join(graph_lines))
+            amr.graph.set_src_tokens(amr.get_src_tokens())
+            amr.misc = misc_lines
+            yield amr
+
+    @staticmethod
     def dump(amr_instances, f):
         for amr in amr_instances:
             f.write(str(amr) + '\n\n')
